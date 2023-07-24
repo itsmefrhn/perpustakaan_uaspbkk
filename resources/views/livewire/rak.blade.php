a9693cc71fcdfeec941d50b63a28d6d2d5faeeab
itsmefrhn <222220030@unmuhpnk.ac.id>
2023-07-24 20:39:15 +0700

    Adding Edit Rak

--- resources/views/livewire/rak.blade.php
+++ resources/views/livewire/rak.blade.php
@@ -3,7 +3,7 @@
 
     @include('admin-lte/flash')
     @include('petugas/rak/create')
-    {{-- @include('petugas/rak/edit') --}}
+    @include('petugas/rak/edit')
     {{-- @include('petugas/kategori/delete') --}}
 
 
