736a98e92f59efa94fc1cba348f1298cd940cde5
itsmefrhn <222220030@unmuhpnk.ac.id>
2023-07-27 17:02:52 +0700

    Adding View Transaksi pada Petugas

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Petugas\RakController;
 use App\Http\Controllers\Petugas\PenerbitController;
 use App\Http\Controllers\Petugas\BukuController;
 use App\Http\Controllers\Peminjam\BukuController as PeminjamBukuController;
+use App\Http\Controllers\Petugas\TransaksiController;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -39,6 +40,7 @@ Route::middleware(['auth', 'role:admin|petugas'])->group(function () {
     Route::get('/rak', RakController::class);
     Route::get('/penerbit', PenerbitController::class);
     Route::get('/buku', BukuController::class);
+    Route::get('/transaksi', TransaksiController::class);
 });
 
 Route::middleware(['auth', 'role:peminjam'])->group(function () {
